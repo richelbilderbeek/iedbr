0c361fe7ec356aa0eb9690976ebcb8ccff344801
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-09 09:25:37 +0100

    'check_mhc_allele_names' now follows a regex

--- R/check_mhc_allele_names.R
+++ R/check_mhc_allele_names.R
@@ -8,9 +8,23 @@
 #' check_mhc_allele_names("cs.{HLA-A*01:01}")
 #' @author Richèl J.C. Bilderbeek
 #' @export
-check_mhc_allele_names <- function(mhc_allele_names) {
+check_mhc_allele_names <- function(
+  mhc_allele_names,
+  mhc_allele_names_regex = "(cs)\\.\\{(HLA-.*)\\}"
+) {
   testthat::expect_equal(length(mhc_allele_names), 1)
-  # Other values are not supported yet
-  testthat::expect_equal(mhc_allele_names, "cs.{HLA-A*01:01}")
+  # Split the string into three
+  # First column: full string if match
+  # Second column: a PostgREST operator,
+  #   from https://postgrest.org/en/v8.0/api.html#operators
+  # Third column: an allele name
+  s <- stringr::str_match(mhc_allele_names, mhc_allele_names_regex)
+  testthat::expect_equal(nrow(s), 1)
+  if (is.na(s[1, 1])) stop(
+    "Invalid value of 'mhc_allele_names': \n",
+    "'mhc_allele_names' does not match the regular expression. \n",
+    "'mhc_allele_names': ", mhc_allele_names, " \n",
+    "'mhc_allele_names_regex': ", mhc_allele_names_regex
+  )
   invisible(mhc_allele_names)
 }
--- tests/testthat/test-check_mhc_allele_names.R
+++ tests/testthat/test-check_mhc_allele_names.R
@@ -1,4 +1,4 @@
-test_that("use", {
+test_that("basic use", {
   expect_silent(check_mhc_allele_names("cs.{HLA-A*01:01}"))
   expect_error(check_mhc_allele_names("nonsense"))
   expect_error(check_mhc_allele_names(NA))
@@ -11,4 +11,50 @@ test_that("use", {
   expect_error(check_mhc_allele_names(c()))
   expect_error(check_mhc_allele_names(list()))
   expect_error(check_mhc_allele_names(rep(0, 2)))
+})
+
+test_that("haplotypes", {
+  # Taken from the bbbq package at https://github.com/richelbilderbeek/bbbq,
+  # From the (incorrectly named) function 'bbbq::get_mhc_haplotypes'
+  # (it should be called 'bbbq::get_mhc_alle_names')
+  many_mhc_allele_names <- c(
+    "HLA-A*01:01",
+    "HLA-A*02:01",
+    "HLA-A*03:01",
+    "HLA-A*24:02",
+    "HLA-A*26:01",
+    "HLA-B*07:02",
+    "HLA-B*08:01",
+    "HLA-B*18:01",
+    "HLA-B*27:05",
+    "HLA-B*39:01",
+    "HLA-B*40:02",
+    "HLA-B*58:01",
+    "HLA-B*15:01",
+    "HLA-DRB1*01:01",
+    "HLA-DRB1*03:01",
+    "HLA-DRB1*04:01",
+    "HLA-DRB1*04:05",
+    "HLA-DRB1*07:01",
+    "HLA-DRB1*08:02",
+    "HLA-DRB1*09:01",
+    "HLA-DRB1*11:01",
+    "HLA-DRB1*12:01",
+    "HLA-DRB1*13:02",
+    "HLA-DRB1*15:01",
+    "HLA-DRB3*01:01",
+    "HLA-DRB3*02:02",
+    "HLA-DRB4*01:01",
+    "HLA-DRB5*01:01",
+    "HLA-DQA1*05:01/DQB1*02:01",
+    "HLA-DQA1*05:01/DQB1*03:01",
+    "HLA-DQA1*03:01/DQB1*03:02",
+    "HLA-DQA1*04:01/DQB1*04:02",
+    "HLA-DQA1*01:01/DQB1*05:01",
+    "HLA-DQA1*01:02/DQB1*06:02"
+  )
+  for (mhc_allele_name in many_mhc_allele_names) {
+    mhc_allele_names <- paste0("cs.{", mhc_allele_name, "}")
+    expect_silent(check_mhc_allele_names(mhc_allele_names))
+  }
 })
